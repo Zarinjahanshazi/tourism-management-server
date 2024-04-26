a399cc535c0ce620076607b886b9ef5017452af2
Zarinjahanshazi <zarinshazi@gmail.com>
2024-04-26 23:09:55 +0600

    setup done

--- index.js
+++ index.js
@@ -2,12 +2,72 @@ require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s8jaol5.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const touristCollection = client.db('touristDB').collection('tourist');
+
+    app.get('/tourist',async(req,res) =>{
+        const cursor = touristCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+    app.get('/tourist/:id',async(req,res) =>{
+        const id = req.params.id;
+        const query ={_id: new ObjectId(id)}
+        const result = await touristCollection.findOne(query);
+        res.send(result);
+
+    })
+
+
+
+    app.post('/tourist',async(req,res) =>{
+        const addTourists = req.body;
+        console.log(addTourists);
+        const result = await touristCollection.insertOne(addTourists);
+        res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/',(req,res) =>{
